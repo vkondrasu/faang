e88fa66af90b7c6d59a56f03db6794d73101c568
vkondrasu <venkat.kondrasu@gmail.com>
2022-02-03 10:44:30 +0530

    LC 454. 4Sum II

---
+++ HashMap/4sumII.py
@@ -0,0 +1,17 @@
+'''
+LC 454. 4Sum II
+'''
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        result = 0
+        m ={}
+        
+        for a in nums1:
+            for b in nums2:
+                m[a+b] = m.get(a+b, 0) + 1
+                
+        for c in nums3:
+            for d in nums4:
+                result  += m.get(-(c+d), 0)
+                
+        return result
