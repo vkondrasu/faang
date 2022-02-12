592c2f1ad077ae2b02318a76b6c83b47c8b9a2de
vkondrasu <venkat.kondrasu@gmail.com>
2022-02-12 17:18:37 +0530

    Added incorrect entries by employees

--- KaratInterview/badgeTimings.py
+++ KaratInterview/badgeTimings.py
@@ -1,4 +1,4 @@
-badge_times = [
+badge_times1 = [
 ["Paul", "1355"],
 ["Jennifer", "1910"],
 ["John", "835"],
@@ -16,6 +16,20 @@ badge_times = [
 ["Jennifer", "730"],
 ["John", "1630"],
 ["Jennifer", "5"]
+]
+
+badge_times =[
+["Paul",      "1355"], ["Jennifer",  "1910"], ["Jose",    "835"],
+  ["Jose",       "830"], ["Paul",      "1315"], ["Chloe",     "0"],
+  ["Chloe",     "1910"], ["Jose",      "1615"], ["Jose",   "1640"],
+  ["Paul",      "1405"], ["Jose",       "855"], ["Jose",    "930"],
+  ["Jose",       "915"], ["Jose",       "730"], ["Jose",    "940"],
+  ["Jennifer",  "1335"], ["Jennifer",   "730"], ["Jose",   "1630"],
+  ["Jennifer",     "5"], ["Chloe",     "1909"], ["Zhang",     "1"],
+  ["Zhang",       "10"], ["Zhang",      "109"], ["Zhang",   "110"],
+  ["Amos",         "1"], ["Amos",         "2"], ["Amos",    "400"],
+  ["Amos",       "500"], ["Amos",       "503"], ["Amos",    "504"],
+  ["Amos",       "601"], ["Amos",       "602"]
 ]
 
 def withinHour(old, new):
@@ -63,4 +77,76 @@ def moreThan3InHour():
             print(user, entry_times[start:i+1])
 
 
-moreThan3InHour()
+#moreThan3InHour()
+
+
+badge_records_1 = [
+  ["Martha",   "exit"],
+  ["Paul",     "enter"],
+  ["Martha",   "enter"],
+  ["Steve",    "enter"],
+  ["Martha",   "exit"],
+  ["Jennifer", "enter"],
+  ["Paul",     "enter"],
+  ["Curtis",   "exit"],
+  ["Curtis",   "enter"],
+  ["Paul",     "exit"],
+  ["Martha",   "enter"],
+  ["Martha",   "exit"],
+  ["Jennifer", "exit"],
+  ["Paul",     "enter"],
+  ["Paul",     "enter"],
+  ["Martha",   "exit"],
+  ["Paul",     "enter"],
+  ["Paul",     "enter"],
+  ["Paul",     "exit"],
+  ["Paul",     "exit"] 
+]
+
+badge_records_2 = [
+  ["Paul", "enter"],
+  ["Paul", "exit"],
+]
+
+badge_records_3 = [
+  ["Paul", "enter"],
+  ["Paul", "enter"],
+  ["Paul", "exit"],
+  ["Paul", "exit"],
+]
+
+badge_records_4 = [
+  ["Paul", "enter"],
+  ["Paul", "exit"],
+  ["Paul", "exit"],
+  ["Paul", "enter"],
+]
+
+def inCorrectEntries(badge_records):
+    mapp = {}
+    exit_before_entry = set()
+    entry_before_exit = set()
+    for user, action in badge_records:
+        if user not in mapp:
+            if action == "exit":
+                exit_before_entry.add(user)
+            mapp[user] = action
+        else:
+            if action == mapp[user]:
+                if action == "enter":
+                    entry_before_exit.add(user)
+                else:
+                    exit_before_entry.add(user)
+            
+            mapp[user] = action
+
+    print("exit_before_entry")
+    print(exit_before_entry)
+    print("entry_before_exit")
+    print(entry_before_exit)
+
+
+inCorrectEntries(badge_records_1)
+#inCorrectEntries(badge_records_2)
+#inCorrectEntries(badge_records_3)
+#inCorrectEntries(badge_records_4)
