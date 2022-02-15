62a792c72075405f821cd7d97ec109aa3fdd088c
vkondrasu <venkat.kondrasu@gmail.com>
2022-02-15 09:39:58 +0530

    LC 136. Single Number

---
+++ BitManipulation/singleNumber.py
@@ -0,0 +1,10 @@
+'''
+LC 136. Single Number
+'''
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a = 0
+        for num in nums:
+            a ^= num
+            
+        return a
