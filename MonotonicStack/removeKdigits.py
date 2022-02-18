26f0c828e63c7b9da8c210689db7dd1bc186b67f
vkondrasu <venkat.kondrasu@gmail.com>
2022-02-18 10:32:26 +0530

    LC 402. Remove K Digits

---
+++ MonotonicStack/removeKdigits.py
@@ -0,0 +1,19 @@
+'''
+LC 402. Remove K Digits
+'''
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        
+        numStack = []
+        
+        for digit in num:
+            while k and numStack and numStack[-1] > digit:
+                numStack.pop()
+                k -= 1
+                
+            numStack.append(digit)
+            
+        finalStack = numStack[:-k] if k else numStack
+        
+        return "".join(finalStack).lstrip('0') or "0"
