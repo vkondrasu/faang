ca2d5b48f59ba622309942b2aeb13e77aff636b4
vkondrasu <venkat.kondrasu@gmail.com>
2022-03-01 17:51:11 +0530

    LC 338. Counting Bits

---
+++ DynamicProgramming/countingBits.py
@@ -0,0 +1,10 @@
+'''
+LC 338. Counting Bits
+'''
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n+1)
+        for x in range(1, n+1):
+            ans[x] = ans[x & (x-1)] + 1
+            
+        return ans
