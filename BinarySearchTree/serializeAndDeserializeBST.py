c10cebee36ff74ad1e25e6167266ecb542e35610
vkondrasu <venkat.kondrasu@gmail.com>
2022-03-23 14:53:25 +0530

    LC 449. Serialize and Deserialize BST

---
+++ BinarySearchTree/serializeAndDeserializeBST.py
@@ -0,0 +1,45 @@
+'''
+LC 449. Serialize and Deserialize BST
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string.
+        """
+        def postorder(root):
+            return postorder(root.left) + postorder(root.right) + [root.val] if root else []
+        
+        return ' '.join(map(str, postorder(root)))
+        
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree.
+        """
+        def helper(lower = -math.inf, upper = math.inf):
+            if not data or data[-1] < lower or data[-1] > upper:
+                return None
+            
+            val = data.pop()
+            root = TreeNode(val)
+            root.right = helper(val, upper)
+            root.left = helper(lower, val)
+            return root
+        
+        data = [int(x) for x in data.split(' ') if x]
+        return helper()
+        
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
