71105b0fc803c12fc30120727d3b69e74e9481e0
vkondrasu <venkat.kondrasu@gmail.com>
2022-03-24 12:10:17 +0530

    LC 48. Rotate Image

---
+++ Arrays/rotateMatrix.py
@@ -0,0 +1,18 @@
+'''
+LC 48. Rotate Image
+'''
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        n = len(matrix[0])
+        
+        for i in range(n//2 + n%2):
+            for j in range(n//2):
+                temp = matrix[n-1-j][i]
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i]
+                matrix[j][n-1-i] = matrix[i][j]
+                matrix[i][j] = temp
