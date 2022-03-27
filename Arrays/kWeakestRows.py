17c3433c82b0586c65d14809072631d226d7a1eb
vkondrasu <venkat.kondrasu@gmail.com>
2022-03-27 23:10:14 +0530

    LC 1337. The K Weakest Rows in a Matrix : vertical iteration, nice approach

--- Arrays/kWeakestRows.py
+++ Arrays/kWeakestRows.py
@@ -1,6 +1,9 @@
 '''
 LC 1337. The K Weakest Rows in a Matrix
 '''
+from unittest import result
+
+
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         new_mat = []
@@ -13,5 +16,35 @@ class Solution:
         result = []
         for summ, i in new_mat[:k]:
             result.append(i)
+            
+        return result
+
+    def kWeakestRowsVerticalIteration(self, mat: List[List[int]], k: int) -> List[int]:
+        rows = len(mat)
+        cols = len(mat[0])
+        
+        result = []
+
+        for i in range(rows):
+            if mat[i][0] == 0 and len(result) < k:
+                result.append(i)
+        
+        if len(result) == k:
+            return result
+        done = False
+        
+        for c in range(1, cols):
+            if done: break
+            for r in range(rows):
+                if len(result) < k and mat[r][c] == 0 and mat[r][c-1] != 0:
+                    result.append(r)
+                elif len(result) == k:
+                    done = True
+                    break
+        i = 0
+        while len(result) < k:
+            if mat[i][-1] == 1:
+                result.append(i)
+            i += 1
             
         return result
